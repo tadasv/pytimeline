60ece63c86aadae9cdd28bf1e482c930a4b8ffe6
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2011-12-11 15:53:34 -0500

    Remove datapoint iterator and container.
    We dont need iterators and containers for datapoints since we have
    custom implementation of Cursor and Collection that will serve low level
    operations.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,7 +4,7 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PYTHONPATH=$DIR/../ python $DIR/datapoint.py
-PYTHONPATH=$DIR/../ python $DIR/session.py
+#PYTHONPATH=$DIR/../ python $DIR/session.py
 PYTHONPATH=$DIR/../ python $DIR/cursor.py
 PYTHONPATH=$DIR/../ python $DIR/collection.py
 PYTHONPATH=$DIR/../ python $DIR/database.py
