9a780511a49fb8fc6fc99d09d6d300520c7af5a5
Tadas Vilkeliskis <vilkeliskis.t@gmail.com>
2011-12-21 22:29:59 -0500

    adding daily name generator.

--- pytimeline/naming.py
+++ pytimeline/naming.py
@@ -1,3 +1,4 @@
+import datetime
 
 def monthly_name(prefix, date):
     """
@@ -33,3 +34,99 @@ def daily_name(prefix, date):
 
     """
     return '%s%d%03d' % (prefix, date.year, date.timetuple().tm_yday)
+
+
+
+class Generator(object):
+    """
+    Name generation interface. Other classes should subclass :class:`Generator`
+    in order to implement custom database/collection name generators.
+
+    """
+
+    def __init__(self, prefix, start, end):
+        """
+
+        :Parameters:
+          - `prefix`: name prefix.
+          - `start`: start date; instance of :class:`datetime.date` or
+            :class:`datetime.datetime`.
+          - `end`: end date; instance of :class:`datetime.date` or
+            :class:`datetime.datetime`.
+
+        """
+
+        assert(isinstance(start, datetime.date) or
+               isinstance(start, datetime.datetime))
+        assert(isinstance(end, datetime.date) or
+               isinstance(end, datetime.datetime))
+        assert(isinstance(prefix, str) or isinstance(prefix, unicode))
+
+        self._start = start
+        self._end = end
+        self._prefix = prefix
+
+
+    @staticmethod
+    def get_name(prefix, date):
+        """
+        Method for getting a name based on the given prefix and date.
+        This method must be implemented by subclasses.
+
+        :Parameters:
+          - `prefix`: name prefix
+          - `date`: date; instance of :class:`datetime.date` or
+            :class:`datetime.datetime`.
+
+        """
+        raise NotImplementedError
+
+
+    @staticmethod
+    def get_day_delta(date):
+        """
+        Method for getting the number of days that must be added
+        to the `date` before generating another name. This method must be
+        implemented bu subclasses.
+
+        :Parameters:
+          - `date`: date; instance of :class:`datetime.date` or
+            :class:`datetime.datetime`.
+
+        """
+        raise NotImplementedError
+
+
+    def generate_names(self):
+        """
+        Generator that generates names in [`start`, `end`] date range.
+
+        """
+        current_date = self._start
+        while current_date <= self._end:
+            yield self.__class__.get_name(self._prefix, current_date)
+            current_date += datetime.timedelta(
+                                days=self.__class__.get_day_delta(current_date))
+
+
+    def get_list(self):
+        """
+        Return a list of names in [`start`, `end`] date range.
+
+        """
+        return list(self.generate_names())
+
+
+class DailyGenerator(Generator):
+    """
+    Generator that generates daily names.
+
+    """
+    @staticmethod
+    def get_name(prefix, date):
+        return daily_name(prefix, date)
+
+
+    @staticmethod
+    def get_day_delta(date):
+        return 1
--- test/naming.py
+++ test/naming.py
@@ -15,5 +15,30 @@ class TestNaming(unittest.TestCase):
                         'p2011032')
 
 
+    def test_name_generators(self):
+        self.assertRaises(AssertionError, naming.Generator, "prefix",
+                            "", datetime.date.today())
+        self.assertRaises(AssertionError, naming.Generator, "prefix",
+                            datetime.date.today(), "")
+        self.assertRaises(AssertionError, naming.Generator, "prefix",
+                            "", datetime.datetime.today())
+        self.assertRaises(AssertionError, naming.Generator, "prefix",
+                            datetime.datetime.today(), "")
+
+
+        # Test daily generator
+        gen = naming.DailyGenerator("p", datetime.date(2011, 1, 28),
+                                         datetime.date(2011, 2, 2))
+        names = gen.get_list()
+        self.assertEqual(names, [
+                            "p2011028",
+                            "p2011029",
+                            "p2011030",
+                            "p2011031",
+                            "p2011032",
+                            "p2011033",
+                        ])
+
+
 if __name__ == '__main__':
     unittest.main()
